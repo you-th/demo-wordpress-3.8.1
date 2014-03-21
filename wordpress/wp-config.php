687e800208f22450f8950956fe789bc10f4391eb
spyy <sami.pyy@gmail.com>
2014-03-21 10:22:36 +0200

    Network setup

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -82,6 +82,13 @@ define('WP_DEBUG', false);
 
 /* Multisite */
 define( 'WP_ALLOW_MULTISITE', true );
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'youth.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 
 /* That's all, stop editing! Happy blogging. */
 
